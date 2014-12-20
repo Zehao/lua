b44172e5d6a791c663b99beb0cb838f3eb320057
zehao <zehaojin@gmail.com>
2014-12-20 22:49:56 +0800

    json字符串转换为table基本ok

--- json.lua
+++ json.lua
@@ -33,13 +33,14 @@ local function char_len(str)
 end 
 
 --[[
-	将utf8格式的unicode字符转换为原始2字节的unicode表示
+	将utf8编码的unicode字符转换为原始2字节的unicode表示
 
 	例如： "金泽豪test123" 转换为 "\x91\xd1\x6c\xfd\x8c\x6atest123"
 
 ]]
 function utf8_to_unicode( str )
 
+	-- 将多个字节的utf8值组合成2字节
 	local  function concat_val( ... )
 		local args = table.pack(...)
 		local res = 0
@@ -61,16 +62,16 @@ function utf8_to_unicode( str )
 	while i <= #str do
 		local cur_byte = string.byte(str,i)
 
-		-- 0000 0000-0000 007F | 0xxxxxxx , 1bit
+		-- 0000 0000-0000 007F | 0xxxxxxx , 1字节
 		if  cur_byte < 0x80 then 
 			res_str[#res_str +1] = string.sub(str,i,i)
 			i = i + 1
 
-		-- 0000 0080-0000 07FF | 110xxxxx 10xxxxxx , 2bits
+		-- 0000 0080-0000 07FF | 110xxxxx 10xxxxxx , 2字节
 		elseif cur_byte < 0xe0 then
 			res_str[#res_str + 1] = concat_val(cur_byte,string.byte(str,i+1))
 			i = i+2
-		-- 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx , 3bits
+		-- 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx , 3字节
 		elseif cur_byte < 0xf0 then
 			res_str[#res_str + 1] =concat_val(cur_byte,string.byte(str,i+1),string.byte(str,i+2))
 			i = i+3
@@ -78,16 +79,78 @@ function utf8_to_unicode( str )
 	end
 	return table.concat(res_str)
 end
+ 
+local  function find_right(str,char,pos)
+	target = "}"
+	if char == "[" then 
+		target = "]"
+	end
+	local count = 1
+	for i = pos+1,#str do
+		if string.sub(str,i,i) == char then count = count +1 end
+		if string.sub(str,i,i) == target then count = count-1 end
+		if count == 0 then
+			return i
+		end
+	end
+	return -1
+end
 
-g_res_tb = {}
+local  function parse_object( raw_json_str,tb_res )
+	if #raw_json_str == 0 then return end
+	json_str= string.match(raw_json_str, "^%s*{(.*)}%s*$" )
+    local pos_left = 1
+	while pos_left < #json_str do
+		key_left,key_right,key = string.find(json_str,"\"(.-)\"%s*:%s*",pos_left)
+		if key_left == nil then return nil,'wrong json string,expected "key": ' end
 
-function marshal(json_str)
-	if(left_pos >= right_pos ) then
-		return
+		key = utf8_to_unicode(key)
+		-- print("key:" .. key)
+
+		value_left_pos = key_right+1
+		value_left_char = string.sub(json_str,value_left_pos,value_left_pos)
+
+
+		-- value is true ,false ,null
+		if value_left_char == "t" or value_left_char == "f" or value_left_char == "n" then  
+			value_left_pos,value_right_pos = string.find(json_str,"%a*",value_left_pos)
+			local tmp_tb = {["true"]=true, ["false"] = false, ["null"] = nil}
+			local value = string.sub(json_str , value_left_pos,value_right_pos)
+			tb_res[key] = tmp_tb[value]
+
+		-- {}
+		elseif value_left_char=="{" then 
+			value_right_pos = find_right(json_str,value_left_char,value_left_pos)
+			tb_res[key] = parse_object(string.sub(json_str,value_left_pos,value_right_pos),{})
+
+		-- []
+		elseif value_left_char =="[" then                             ---- []
+			value_right_pos = find_right(json_str,value_left_char,value_left_pos)
+			tb_res[key] = parse_array(string.sub(json_str,value_left_pos,value_right_pos),{})
+
+		-- string
+		elseif value_left_char == '"' then
+			value_left_pos,value_right_pos,value = string.find(json_str,"\"(.-)\"",value_left_pos)
+			-- print("string value:" , value)
+			tb_res[key] = utf8_to_unicode (value)
+
+		-- numbers
+		else
+			value_left_pos,value_right_pos,value = string.find(json_str,"([^,%s]+)",value_left_pos)
+			tb_res[key] = tonumber(value)
+		end
+
+		pos_left = value_right_pos + 1
+		pos_left = select(2,string.find(json_str,"[%s,]*",pos_left))
+		pos_left = pos_left + 1
 	end
 
-	if json_str[left_pos] == "{" then
-	end
+	return tb_res
+end 
+
+
+local function parse_array( json_str,tb_res )
+	-- body
 end
 
 
@@ -95,10 +158,22 @@ function Marshal(json_str)
 	if json_str == nil or json_str == "" then
 		return nil,"null input"
 	end
+	--将原始串的回车，换行全部去掉
+	json_str = string.gsub(json_str,"[%c]","")
 
-	res = marshal(json_str)
+	--是{还是[开头
+	str_type = string.match(json_str,"^%s*([\\[{])")
 
-	-- 
+	local tb_res ={}
+	if str_type == "{" then
+		tb_res,message = parse_object(json_str,tb_res)
+	elseif str_type == "[" then
+		tb_res,message = parse_array(json_str,tb_res)
+	else 
+		return nil,"{ or [ expected"
+	end
+
+	return tb_res,message
 end
 
 
--- test.lua
+++ test.lua
@@ -1,5 +1,57 @@
 json = require("json")
 
-a="金泽豪test123"
+--a="金泽豪test123"
 
-print(json.utf8_to_unicode(a))
+--print(json.utf8_to_unicode(a))
+
+json_str1 = [[
+
+	{
+		"test":1 ,
+		"test2":2   ,
+		"金泽豪":"呵呵",
+
+		"又一个table" : { "tb1":1, "tb2":2}
+	}
+
+
+]]
+	
+
+
+-- 按不同缩进递归打印table
+local function print_table( tb ,level)
+	level = level or 0
+	for k,v in pairs(tb) do
+		io.write(string.rep("\t",level))
+		io.write("key:" .. k .. ",")
+		if type(v) == "table" then
+			io.write("\n")
+			print_table(v,level + 1)
+		else 
+			io.write("value:" .. v .. "\n")
+		end
+	end
+end
+
+
+
+tb = json.Marshal(json_str1)
+
+print_table(tb)
+
+
+--[[
+
+
+
+     {  "bindings" :  [ 
+				{"ircEvent": "PRIVMSG", "method": "newURI", "regex": "^http://.*"}, 
+				{"ircEvent": "PRIVMSG", "method": "deleteURI", "regex": "^delete.*"}, 
+				{"ircEvent": "PRIVMSG", "method": "randomURI", "regex": "^random.*"} 
+		]	
+	}    
+
+
+
+]]
